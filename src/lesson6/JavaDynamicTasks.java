808da7cdb80e1bdc99f931983476f9e7e4c09519
farruk12 <988530316f>
2018-12-14 20:45:50 +0300

    second method

--- src/lesson6/JavaDynamicTasks.java
+++ src/lesson6/JavaDynamicTasks.java
@@ -2,14 +2,19 @@ package lesson6;
 
 import kotlin.NotImplementedError;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaDynamicTasks {
     /**
      * Наибольшая общая подпоследовательность.
      * Средняя
-     *
+     * <p>
      * Дано две строки, например "nematode knowledge" и "empty bottle".
      * Найти их самую длинную общую подпоследовательность -- в примере это "emt ole".
      * Подпоследовательность отличается от подстроки тем, что её символы не обязаны идти подряд
@@ -24,7 +29,7 @@ public class JavaDynamicTasks {
     /**
      * Наибольшая возрастающая подпоследовательность
      * Средняя
-     *
+     * <p>
      * Дан список целых чисел, например, [2 8 5 9 12 6].
      * Найти в нём самую длинную возрастающую подпоследовательность.
      * Элементы подпоследовательности не обязаны идти подряд,
@@ -33,31 +38,66 @@ public class JavaDynamicTasks {
      * то вернуть ту, в которой числа расположены раньше (приоритет имеют первые числа).
      * В примере ответами являются 2, 8, 9, 12 или 2, 5, 9, 12 -- выбираем первую из них.
      */
+    // Трудоемкость: T = O(n * n)
+    // Ресурсоемкость: R = O(n)
     public static List<Integer> longestIncreasingSubSequence(List<Integer> list) {
-        throw new NotImplementedError();
+        int n = list.size();
+        if (n == 0 || n == 1) return list;
+        int[] prev = new int[n];
+        int[] d = new int[n];
+
+        Arrays.fill(d, 1);
+        Arrays.fill(prev, -1);
+
+        int pos = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (list.get(j) < list.get(i) && d[j] + 1 > d[i]) {
+                    d[i] = d[j] + 1;
+                    prev[i] = j;
+                    if (d[pos] < d[i]) {
+                        pos = i;
+                    }
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        int index = pos;
+        while (index >= 0) {
+            result.add(list.get(index));
+            index = prev[index];
+        }
+        List<Integer> answer = new ArrayList<>();
+        for(int i = result.size() - 1; i >= 0; i--){
+            answer.add(result.get(i));
+        }
+        return answer;
     }
 
     /**
      * Самый короткий маршрут на прямоугольном поле.
      * Сложная
-     *
+     * <p>
      * В файле с именем inputName задано прямоугольное поле:
-     *
+     * <p>
      * 0 2 3 2 4 1
      * 1 5 3 4 6 2
      * 2 6 2 5 1 3
      * 1 4 3 2 6 2
      * 4 2 3 1 5 0
-     *
+     * <p>
      * Можно совершать шаги длиной в одну клетку вправо, вниз или по диагонали вправо-вниз.
      * В каждой клетке записано некоторое натуральное число или нуль.
      * Необходимо попасть из верхней левой клетки в правую нижнюю.
      * Вес маршрута вычисляется как сумма чисел со всех посещенных клеток.
      * Необходимо найти маршрут с минимальным весом и вернуть этот минимальный вес.
-     *
+     * <p>
      * Здесь ответ 2 + 3 + 4 + 1 + 2 = 12
      */
-    public static int shortestPathOnField(String inputName) {
+    public static int shortestPathOnField(String inputName) throws IOException {
+        File file = new File(inputName);
+        BufferedReader fr = new BufferedReader(new FileReader(file));
+        String line = fr.readLine();
         throw new NotImplementedError();
     }
 
